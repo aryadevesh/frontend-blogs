163a71bdfd64997b0c941c094f0ec7dd0b5f258e
Devesh Kumar Arya <aryadevesh78@gmail.com>
2024-08-30 00:17:44 +0530

    minor change

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -9,7 +9,7 @@ export const Appbar = () => {
         <div className="grid grid-cols-2">
             <div>
                 <Link to={`/publish`}>
-                    <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Add Paper</button>
+                    <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Add Blog</button>
                 </Link>
             </div>
             <div className="flex justify-end">
